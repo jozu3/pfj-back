a80e970b0ed8d562ba6b6e75735db270ac10ce58
Marjorie <marjorieynumarivera@gmail.com>
2022-04-29 23:23:10 -0500

    Administrar InscripcioneTarea en el administrador y consejero

--- app/Http/Livewire/Student/CreateLectura.php
+++ app/Http/Livewire/Student/CreateLectura.php
@@ -9,15 +9,14 @@ use Livewire\Component;
 
 class CreateLectura extends Component
 {
-    public $programa;
-    public $checkTarea = "esto es un texto";
+    public $programa;    
 
     public function render()
     {
         $programa = $this->programa;
         $inscripcione = Inscripcione::where('personale_id', auth()->user()->personale->id)->where('programa_id', $this->programa->id)->first();
-        $inscripcioneTarea = InscripcioneTarea::where('inscripcione_id', $inscripcione->id)->get();
-        return view('livewire.student.create-lectura', compact('programa', 'inscripcioneTarea'));
+        $inscripcioneTareas = InscripcioneTarea::where('inscripcione_id', $inscripcione->id)->get();
+        return view('livewire.student.create-lectura', compact('programa', 'inscripcioneTareas'));
     }
 
     public function realizado($tarea)
@@ -26,9 +25,8 @@ class CreateLectura extends Component
 
         $inscripcioneTarea = InscripcioneTarea::where('inscripcione_id', $inscripcione->id)->where('tarea_id', $tarea)->first();
 
-        if ($inscripcioneTarea) {
-            $this->checkTarea = !$inscripcioneTarea->realizado;
-            $inscripcioneTarea->realizado = $this->checkTarea;
+        if ($inscripcioneTarea) {            
+            $inscripcioneTarea->realizado = !$inscripcioneTarea->realizado;
 
         } else {
             $inscripcioneTarea = new InscripcioneTarea([
@@ -39,9 +37,6 @@ class CreateLectura extends Component
 
         }
 
-        $inscripcioneTarea->save();
-
-        $this->checkTarea = 'Cambio relaizado';
-
+        $inscripcioneTarea->save();        
     }
 }
--- resources/views/admin/programas/partials/lectura.blade.php
+++ resources/views/admin/programas/partials/lectura.blade.php
@@ -1,6 +1,60 @@
+<div>
+    <div class="card">
+        <div class="card-header">
+            <b>Lecturas del personal</b>
+        </div>
+        <div class="card-body">
+            <div>
+                {{-- <label for="">Buscar personal</label> --}}
+                <input wire:model="search" class="form-control form-control-sm"
+                    placeholder="Ingrese nombre o apellido de un personal">
+            </div>
+            <div>
+                <table class="table table-striped">
+                    <thead>
+                        <tr>
+                            <th>Apellidos</th>
+                            <th>Nombres</th>
+                            @forelse ($programa->tareas->sortBy('fecha') as $tarea)
+                                <th class="text-center">{{ $tarea->descripcion }}</th>
+                            @empty
+                                <th>No hay lecturas</th>
+                            @endforelse
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($programa->inscripcionesEstado([0, 1, 2]) as $inscripcione)
+                            <tr>
+                                <td>
+                                    <b>{{ $inscripcione->personale->contacto->apellidos }}</b>
+                                </td>
+                                <td>
+                                    <b>{{ $inscripcione->personale->contacto->nombres }}</b>
+                                </td>
+                                @forelse ($programa->tareas->sortBy('fecha') as $tarea)                                    
+                                    <td class="text-center">
+                                        @livewire('admin.create-inscripcione-tarea',
+                                        ['tarea_id' => $tarea->id, 'inscripcione_id' => $inscripcione->id])
+                                    </td>
+                                @empty
+                                    <td>No hay lecturas</td>
+                                @endforelse
+                            </tr>
+                        @endforeach
+                        {{-- {{ $programa }} --}}
+                        {{-- @forelse ($collection as $item)
+                        <tr>
+                            <td></td>
+                        </tr>
+                        @empty
+                            
+                        @endforelse --}}
 
-    <!-- /.card-header -->
-   
-            {{-- @livewire('admin.tarea-lista', ['programa' => $programa]) --}}
-        
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
 
+{{-- @livewire('admin.tarea-lista', ['programa' => $programa]) --}}
